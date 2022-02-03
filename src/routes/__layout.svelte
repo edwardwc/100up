938cf1984636ce6a4bf5443f408ed72cf5e4b5b8
ewc <edwardwc@protonmail.com>
2022-02-03 08:13:46 -0500

    fix: browser-forced darkmode, favicon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,7 @@
 <svelte:head>
 	<title>100up: Real-Time Attack Statistics</title>
 </svelte:head>
-<html data-theme="dark" lang='en'>
+<html data-theme="dark" lang='en' class='dark:text-white'>
 	<div class="bg-[url('/topography.svg')]">
 		<slot></slot>
 	</div>
